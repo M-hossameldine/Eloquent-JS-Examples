bc00a913bf0ed7c65d1eeb989911b6d8abcd239b
Mohamed Hossam <m.hossameldine@gmail.com>
2021-05-26 18:56:19 +0200

    Classes V.2

--- ch_6-The_Secret_Life_Of_Objects/Playground/f6.3-Classes.js
+++ ch_6-The_Secret_Life_Of_Objects/Playground/f6.3-Classes.js
@@ -57,50 +57,5 @@ console.log(Object.getPrototypeOf(blackRabbit) == Rabbit.prototype);
 // -> true
 
 
-// Third approach - Explicitly using the class notation 
-/*
- * This apporach used speacially, we you plan to use the class with anther object-oriented concepts like inheritance
-*/ 
-
-class CAR {
-  name;
-  year;
-  price;
-
-  constructor (name, year, price) {
-    this.name = name;
-    this.year = year;
-    this.price = price;
-  }
-
-  print () {
-    console.log(`The ${this.name} car was manufactured at ${this.year} and priced with ${this.price}`)
-  } 
-}
-
-let toyota = new CAR ("toyota", 1995, 198000);
-toyota.print();
-
-//  Function Approach for the class car
-function CreateCar (name, year, price) {
-  let holder = {
-    name: undefined,
-    year: undefined,
-    price: undefined,
-    print() {
-      console.log(`This ${this.name} car was manufactured in ${this.year} and valued ${this.price} `)
-    }
-  };
-
-  holder.name = name;
-  holder.year = year;
-  holder.price = price;
-
-  return holder;
-}
-
-let yellowCar = new CreateCar ("JAGUAR", "2000", "6895000");
-yellowCar.print();
-
 
 
--- ch_6-The_Secret_Life_Of_Objects/Playground/f6.4-Class_Notatoin.js
+++ ch_6-The_Secret_Life_Of_Objects/Playground/f6.4-Class_Notatoin.js
@@ -14,3 +14,49 @@ console.log(happyRabbit.speak("Cheeeese!"));
 // class can be used in both statments and expressions
 let welcome = new class {sayHello() {return "HelloOo!"}};
 console.log(welcome.sayHello());
+
+// Third approach - Explicitly using the class notation 
+/*
+ * This apporach used speacially, we you plan to use the class with anther object-oriented concepts like inheritance
+*/ 
+
+class CAR {
+  name;
+  year;
+  price;
+
+  constructor (name, year, price) {
+    this.name = name;
+    this.year = year;
+    this.price = price;
+  }
+
+  print () {
+    console.log(`The ${this.name} car was manufactured at ${this.year} and priced with ${this.price}`)
+  } 
+}
+
+let toyota = new CAR ("toyota", 1995, 198000);
+toyota.print();
+
+//  Function Approach for the class car
+function CreateCar (name, year, price) {
+  let holder = {
+    name: undefined,
+    year: undefined,
+    price: undefined,
+    print() {
+      console.log(`This ${this.name} car was manufactured in ${this.year} and valued ${this.price} `)
+    }
+  };
+
+  holder.name = name;
+  holder.year = year;
+  holder.price = price;
+
+  return holder;
+}
+
+let yellowCar = new CreateCar ("JAGUAR", "2000", "6895000");
+yellowCar.print();
+
