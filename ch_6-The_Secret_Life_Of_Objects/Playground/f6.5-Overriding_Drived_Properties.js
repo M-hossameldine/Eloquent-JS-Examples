45d2f3e65e90842db1dca8c8f47abab494544b7a
Mohamed Hossam <m.hossameldine@gmail.com>
2021-05-28 03:11:18 +0200

    Overriding Drived properties V.2

--- ch_6-The_Secret_Life_Of_Objects/Playground/f6.5-Overriding_Drived_Properties.js
+++ ch_6-The_Secret_Life_Of_Objects/Playground/f6.5-Overriding_Drived_Properties.js
@@ -3,9 +3,17 @@ killerRabbit.teeth = "sharp, large";
 
 let blackRbbit = Object.create(protoRabbit);
 
-console.log(killerRabbit.teeth + teeth); // -> Sharp, large
+console.log(killerRabbit.teeth , "teeth"); // -> Sharp, large
 console.log(blackRbbit.teeth);           // -> small
 
 /*
  * Overriding is also used to give the standard function and array prototypes a different toString method than the basic object prototype
 */
+
+console.log(Object.prototype.toString == Function.prototype.toString);
+//-> false
+console.log([1,2].toString()); // has the same result as join(', ')
+// -> 1, 2
+console.log(Object.prototype.toString.call([1, 2]));
+// -> [object Array]  - the invocated toString method is not designed to be used for arrays - 
+
