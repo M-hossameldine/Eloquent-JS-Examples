b60077b9f02f57fd1cbe2cd19ecdbe7e8c657cfd
Mohamed Hossam <m.hossameldine@gmail.com>
2021-06-21 02:28:06 +0200

    The Iterator interface - full version

--- ch_6-The_Secret_Life_Of_Objects/Playground/f6.9-The_Iterator_Interface.js
+++ ch_6-The_Secret_Life_Of_Objects/Playground/f6.9-The_Iterator_Interface.js
@@ -1,9 +1,18 @@
+// links
+/*
+  -Symbol.iterator
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol/iterator
+
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/@@iterator
+
+*/
+
 // Symbol Iterator & For..Of Loop /
 /*
 (Iterable) -> Object has Symbol iterator
 -> Object with interface designed for iteration
 -> All iterators has Next() method that returns result object
--> The result object has 2 prop :
+-> The result object has 2 properties :
 [1] value - The next value
 [2] Done - Boolean value to tell if the next value exists or not
 -> ( String - Array ) Accept Iteration /  (Number - Object) Not Accept
@@ -27,3 +36,88 @@
     which should be true when there are no more results and false otherwise
 */
 
+// Implement the iterator interface manually
+  let okIterator = "ok"[Symbol.iterator]();
+  console.log(okIterator.next());
+  // -> {value: 'o', done: false}
+  console.log(okIterator.next());
+  // -> {value: 'k', done: false}
+  console.log(okIterator.next());
+  // -> {value: undefined, done: true}
+
+
+// links 
+/*
+  https://stackoverflow.com/questions/28739745/how-to-make-an-iterator-out-of-an-es6-class
+  
+*/
+// Implement the iterable data structure
+/*
+  - We will build a matrix class, acting as a two dimentional array,
+    using the following equation for the index of each element ( y * width + x )
+  - This Class stores it's content into a single array of width * height elements
+  - The constructor takes 3 parameters; width, height, and an optional function that will be used to fill in the first values  
+*/
+class Matrix {
+  constructor (width, height, element = (x, y) => undefined){
+    this.width = width;
+    this.height = height;
+    this.content = [];
+
+    for (let y = 0; y < height; y++) {
+      for(let x = 0; x < width; x++) {
+        this.content[y * width + x] = element(x, y);
+      }
+    }
+  }
+
+  get(x, y) {
+    return this.content[y * this.width + x];
+  }
+
+  set (x, y, value) {
+    this.content[y * this.width + x] = value
+  }
+
+}
+
+class MatrixIterator {
+  constructor(matrix) {
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+
+  next() {
+    if(this.y == this.matrix.height) return {done: true};
+
+    let value = { x: this.x,
+                  y: this.y,
+                  value: this.matrix.get(this.x, this.y)
+                }
+    this.x++;
+    if(this.x == this.matrix.width) {
+      this.x = 0;
+      this.y++;
+    }
+
+    return {value, done: false}
+  }
+}
+
+Matrix.prototype[Symbol.iterator] = function ( ) {
+  return new MatrixIterator(this);
+}
+
+let myMatrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
+for( let {x, y, value} of myMatrix) {
+  console.log(x, y, value);
+}
+
+/*
+  logs:
+  0 0 value 0, 0
+  0 1 value 0, 1
+  1 0 value 1, 0
+  1 1 value 1, 1
+*/
