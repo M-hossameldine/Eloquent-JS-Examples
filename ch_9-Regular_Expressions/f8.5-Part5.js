082c19f6673593a91bb7396f94ea9207744cc7b5
Mohamed Hossam <m.hossameldine@gmail.com>
2021-09-03 02:10:45 +0200

    International Characters

--- ch_9-Regular_Expressions/f8.5-Part5.js
+++ ch_9-Regular_Expressions/f8.5-Part5.js
@@ -1,5 +1,12 @@
 /*
 * Parasing an INI File
+* International Characters
+  - Non-English Characters
+  - Two-Unit Characters
+  - u flag
+  - \p
+    \p{Property=Value}
+    \p{Property}
 
 */
 
@@ -29,4 +36,31 @@ function parseINI(string) {
   });
   return result;
 }
+/**************************************************************/
+// International Characters
+/**************************************************************/
+/*
+* JavaScript's RE are rather dumb about characters that do not apppear in the English language
+* RE work on code units (Ch. 5). This means characters that are composed of two code units behave strangely.
+*/
+
+console.log(/🍎{3}/.test("🍎🍎🍎")); // -> false
+// The dot matches a single code unit, not the two that make up the rose emoji
+console.log(/<.>/.test("<🌹>"));       // -> false
+// 'u' option (for Unicode) to your RE to make it treat such (two unit) characters properly
+console.log(/<.>/u.test("<🌹>"));      // -> true
+// '\p' in a RE (that must have the unicode option enabled) to match all characters to which the Unicode standard assigns a given property 
+console.log(/\p{Script=Greek}/u.test("α"));   // -> true
+console.log(/\p{Script=Arabic}/u.test("α"));  // -> false
+console.log(/\p{Script=Arabic}/u.test("ب"));  // -> true
+console.log(/\p{Alphabetic}/u.test("!"));     // -> false
+console.log(/\p{Alphabetic}/u.test("a"));     // -> true
+console.log(/\p{Alphabetic}/u.test("أ"));     // -> true
+/*
+\p{Property=Value}
+ - To match any character that has the given value for that property.
+\p{Propery}
+ - The value is assumed to be either a binary property such as Alphabetic or a category such as Number 
+
+*/
 /**************************************************************/
